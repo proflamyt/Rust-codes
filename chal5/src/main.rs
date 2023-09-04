4f2825733beaaecc7ae8b84aeecb4fd633a8d257
proflamyt <53262578+proflamyt@users.noreply.github.com>
2023-09-04 08:49:21 +0100

    add later

--- chal5/src/main.rs
+++ chal5/src/main.rs
@@ -1,24 +1,55 @@
-// use base64::{Engine as _, engine::general_purpose};
-
-use std::u8;
+use base64::{Engine as _, engine::general_purpose};
+use std::fs::File;
+use std::io::{self, BufRead};
 
 
 fn main() {
 
-    // let orig: &[u8; 4] = b"data";
-    // let decoded:Vec<u8>  = general_purpose::STANDARD_NO_PAD.decode(orig).unwrap();
-    // println!("{:?}", decoded);
-    let my_string: String = String::from("this is a test");
-    let sec_string: String =  String::from("wokka wokka!!!");
-    let c: Option<u32> =  hamming_distance(my_string.as_bytes(), sec_string.as_bytes());
+    let my_str = read_file();
+    let bytes = general_purpose::STANDARD.decode(&my_str).unwrap();
+    let mut averages =  Vec::new();
+    
 
-    match c {
-        Some(distance) => {
-            // Convert the Vec<u8> to a hexadecimal string
-            
-            println!("Hamming distance is ...: {}", distance);
+
+    for keysize in 2..40 {
+
+        if keysize > my_str.len() || 2*keysize > my_str.len() {
+            break;
         }
-        None => println!("Error: Vectors must have equal lengths to compute XOR."),
+
+        let mut norm: Vec<u32> = Vec::new();
+
+        let block_bytes = split_bytes_into_blocks(&bytes, keysize, true);
+
+        for i in 0..block_bytes.len() {
+            let c: Option<u32> =  hamming_distance(&block_bytes[i], &block_bytes[i+1]);
+            match c {
+                Some(distance) => {
+                    // Convert the Vec<u8> to a hexadecimal string
+                    
+                    let normalized: u32 = distance/(keysize as u32);
+                    norm.push(normalized);
+    
+                    // find smallest
+                    
+                }
+                None => println!("Error: Vectors must have equal lengths to compute XOR."),
+            }
+        }
+        let sum: u32 = norm.iter().sum();
+        let average: u32 = sum / norm.len() as u32;
+        averages.push(average);
+
+        norm.clear();
+        
+        println!("Hamming distance is ...: {:?}", norm);
+    }
+    if let Some((min_index, min_value)) = &averages.iter().enumerate().min_by_key(|(_, &x)| x) {
+        println!("Numbers: {:?}", &averages);
+        println!("Smallest Number: {}", min_value);
+        println!("Index of Smallest Number: {}", min_index+2);
+    } else {
+        println!("The vector is empty.");
     }
 
 
@@ -39,4 +70,48 @@ fn hamming_distance(a:&[u8], b:&[u8]) ->  Option<u32> {
         result += xor_result.count_ones();
     }
     Some(result)
+}
+
+
+fn read_file() -> String{
+    let file = File::open("file6.txt").unwrap();
+    let mut base64_file = String::new();
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        let line_content: String = line.unwrap();
+        base64_file += &line_content;
+    }
+   
+    base64_file
+    
+}
+
+
+fn split_bytes_into_blocks(data: &[u8], block_size: usize, instance: bool) -> Vec<Vec<u8>> {
+    let mut blocks = Vec::new();
+    let mut current_block = Vec::with_capacity(block_size);
+    let mut count = 0;
+
+    for &byte in data {
+        current_block.push(byte);
+
+        if current_block.len() == block_size {
+            blocks.push(current_block.clone()); // Clone to create a new block
+            current_block.clear();
+        }
+
+        if instance && count == 4 {
+            return blocks;
+        }
+
+        count += 1;
+    }
+
+    // Add any remaining bytes as the last block
+    if !current_block.is_empty() {
+        blocks.push(current_block);
+    }
+
+    blocks
 }
