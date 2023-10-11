1ccab63c74ac2a64be0033c9f8fe8665e9a7c8e1
proflamyt <53262578+proflamyt@users.noreply.github.com>
2023-10-11 17:03:30 +0100

    adding detection oracle

---
+++ detection_oracle/src/main.rs
@@ -0,0 +1,42 @@
+use rand::Rng;
+
+
+
+fn main() {
+    let my_string = "Olamide";
+    let my_bytes = my_string.as_bytes();
+    let key = rand_key();
+}
+
+fn encryption_oracle(input: String) {
+
+    let random_bit: u8 = rand::thread_rng().gen_range(0..=1);
+}
+
+
+
+fn pad_plaintext(plaintext: &[u8]) -> Vec<u8> {
+    let mut rng = rand::thread_rng();
+    let mut random_bytes = Vec::from(plaintext);
+
+    for i in 0..2 { // Generate two random byte arrays
+        let length = rng.gen_range(5..=10);
+        let mut random_bytes_part = vec![0u8; length];
+        rng.fill(random_bytes_part.as_mut_slice());
+        if i == 0 {
+            random_bytes.extend_from_slice(&random_bytes_part);
+        }
+        else {
+            random_bytes.splice(0..0, random_bytes_part.clone());
+        }
+    }
+
+    random_bytes
+}
+
+
+fn rand_key() -> [u8; 16] {
+    let random_bytes = rand::thread_rng().gen::<[u8; 16]>();
+    println!("{:?}", random_bytes);
+    random_bytes
+}
